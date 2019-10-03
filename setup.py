42286165a8cbf90890560d0c471dd24b9f7f051f
chb69 <chb69@pitt.edu>
2019-10-03 15:09:24 -0400

    adding LICENSE and setup.py for pip installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="hubmap-commons",
+    version="0.0.1",
+    author="Chuck Borromeo",
+    author_email="chb69@pitt.edu",
+    description="The common tools required by the HuMBAP web services",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/hubmapconsortium/commons",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
