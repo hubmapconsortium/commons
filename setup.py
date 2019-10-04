b31c3ca944cd6e1475abc3daa4412b77392fc43c
chb69 <chb69@pitt.edu>
2019-10-03 20:52:21 -0400

    updated name of hubmap_commons package

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hubmap-commons",
+    name="hubmap_commons",
     version="0.0.1",
     author="Chuck Borromeo",
     author_email="chb69@pitt.edu",
