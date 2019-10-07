0a5e046fc917b78fa1232a5cb43a0b0d1a334507
chb69 <chb69@pitt.edu>
2019-10-07 09:22:56 -0400

    changed package name from hubmapcommons to hubmap-commons

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="hubmap_commons",
+    name="hubmap-commons",
     version="0.0.1",
     author="Chuck Borromeo",
     author_email="chb69@pitt.edu",
