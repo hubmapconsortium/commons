3febc92b3704c2c6d3bc2df57dc02b472c627621
chb69 <chb69@pitt.edu>
2019-10-28 15:38:47 -0400

    changed constructor to explicitly set the message variable

--- hubmap_commons/hubmap_error.py
+++ hubmap_commons/hubmap_error.py
@@ -3,13 +3,14 @@ import logging
 
 
 class HubmapError(Exception):
-    def __init__(self, message, errors):
+    def __init__(self, message, errors=''):
 
         # Call the base class constructor with the parameters it needs
         super().__init__(message)
 
         # TODO maybe check if errors is None.  If it is None, try using traceback 
         self.errors = errors
+        self.message = message
         
     def getJson(self):
         return {'displayMessage' : self.message, 'internalMessage' : self.errors}
