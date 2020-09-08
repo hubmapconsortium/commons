872799f682c69cbf9642395c51bcaf38ade3766c
Zhou (Joe) Yuan <yuanzhou19@gmail.com>
2020-09-08 11:56:26 -0400

    Bumped package version to 1.12.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="hubmap-commons",
-    version="1.12.2",
+    version="1.12.3",
     author="Chuck Borromeo",
     author_email="chb69@pitt.edu",
     description="The common tools required by the HuMBAP web services",
