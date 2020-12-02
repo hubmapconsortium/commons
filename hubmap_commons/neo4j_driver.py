a391f43f78b6e27699932414e15aa822d01dd926
yuanzhou <yuanzhou19@gmail.com>
2020-12-02 17:16:26 -0500

    Add var definition and func comment

--- hubmap_commons/neo4j_driver.py
+++ hubmap_commons/neo4j_driver.py
@@ -3,7 +3,26 @@ from neo4j import GraphDatabase
 # Python modules are first-class runtime objects, 
 # they effectively become singletons, 
 # initialized at the time of first import.
-def initialize(uri, user, password):
+__NEO4J_DRIVER__ = None
+
+"""
+Create a neo4j.Driver singleton instance
+
+Parameters
+----------
+uri : str
+    Neo4j server uri, bolt://host[:port]
+username : str
+    Neo4j username
+password : str
+    Neo4j password
+
+Returns
+-------
+neo4j.Driver
+    A neo4j.Driver instance
+"""
+def initialize(uri, username, password):
     # Two leading underscores signals to Python that 
     # you want the variable to be "private" to the module
     global __NEO4J_DRIVER__
@@ -11,6 +30,6 @@ def initialize(uri, user, password):
     if __NEO4J_DRIVER__ is not None:
         raise RuntimeError("You cannot create another neo4j_driver instance")
 
-    __NEO4J_DRIVER__ = GraphDatabase.driver(uri, auth=(user, password))
+    __NEO4J_DRIVER__ = GraphDatabase.driver(uri, auth=(username, password))
 
     return __NEO4J_DRIVER__
