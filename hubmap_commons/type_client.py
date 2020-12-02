d2ef8410a750a5988e47e5b50043e508b98aabe0
Joel Welling <welling@psc.edu>
2020-12-02 18:59:43 -0500

    flake8 fixes to type_client.py and test_type_client.py

--- hubmap_commons/type_client.py
+++ hubmap_commons/type_client.py
@@ -6,6 +6,7 @@ Created on November 22, 2020
 
 import requests
 import json
+import yaml
 from typing import Union, List, Iterable, Dict, Any
 from requests.exceptions import TooManyRedirects
 from pprint import pprint
@@ -16,6 +17,82 @@ BoolOrNone = Union[bool, None]
 StringOrNone = Union[str, None]
 
 JSONType = Union[str, int, float, bool, None, Dict[str, Any], List[Any]]
+
+"""Some example type data for use by the dummy type client"""
+DUMMY_CLIENT_YAML = """
+salmon_rnaseq_bulk:
+#RNAseq-bulk_salmon:
+    description: Bulk RNA-seq [Salmon]
+#    alt-names: [salmon_rnaseq_bulk]
+    alt-names: []
+    primary: false
+    vitessce-hints: []
+
+CODEX:
+    description: CODEX
+    alt-names: []
+    primary: true
+    vitessce-hints: []
+
+codex_cytokit:
+    description: CODEX [Cytokit + SPRM]
+    alt-names: []
+    primary: false
+    vitessce-hints: ['codex', 'is_image', 'is_tiled']
+
+image_pyramid:
+    description: Image Pyramid
+    alt-names: []
+    primary: false
+    vitessce-hints: ['is_image', 'pyramid']
+
+SNAREseq:
+    description: SNARE-seq
+    alt-names: []
+    primary: true
+    vitessce-hints: []
+
+sc_atac_seq_snare_lab:
+#SNAREseq_lab:
+    description: scATAC-seq (SNARE-seq) [Lab Processed]
+#    alt-names: [sc_atac_seq_snare_lab]
+    alt-names: []
+    primary: false
+    vitessce-hints: []
+
+sc_atac_seq_snare:
+#SNAREseq_snapATAC:
+    description: scATAC-seq (SNARE-seq) [SnapATAC]
+#    alt-names: [sc_atac_seq_snare]
+    alt-names: []
+    primary: false
+    vitessce-hints: ['is_sc', 'atac']
+
+scRNA-Seq-10x:
+    description: scRNA-seq (10x Genomics)
+    alt-names: []
+    primary: true
+    vitessce-hints: []
+
+scRNA-Seq-10x_salmon:
+    description: scRNA-seq (10x Genomics) [Salmon]
+    alt-names: [salmon_rnaseq_10x]
+    primary: false
+    vitessce-hints: ['is_sc', 'rna']
+
+PAS:
+    description: PAS Stained Microscopy
+    alt-names: []
+    primary: true
+    vitessce-hints: []
+
+PAS_pyramid:
+    description: PAS Stained Microscopy [Image Pyramid]
+    alt-names: [["PAS", "Image Pyramid"], ["Image Pyramid", "PAS"]]
+    primary: false
+    vitessce-hints: ['is_image', 'pyramid']
+
+"""
 
 
 class _AssayType(object):
@@ -36,8 +113,9 @@ class _AssayType(object):
         self.name = info['name']
         self.description = info['description']
         self.primary = info['primary']
-        self.vitessce_hints = (info['vitessce-hints'] if 'vitessce-hints' in info
-                              else [])
+        self.vitessce_hints = (info['vitessce-hints']
+                               if 'vitessce-hints' in info
+                               else [])
 
     def to_json(self) -> Dict[str, Any]:
         """
@@ -182,3 +260,89 @@ class TypeClient(object, metaclass=SingletonMetaClass):
         for elt in data['result']:
             assert isinstance(elt, Dict)
             yield _AssayType(elt)
+
+
+class DummyTypeClient(object, metaclass=SingletonMetaClass):
+    """
+    This local-only version of TypeClient is provided for testing purposes.
+
+    This is a singleton- only a single instance of this class is ever created.
+    If the constructor is called again, it returns the same instance as before.
+    This is for convenience in initializing the service.
+    """
+    app_config: Dict[str, Any]
+
+    def __init__(self, type_webservice_url: StringOrNone = None):
+        """
+        type_webservice_url must be provided the first time the constructor is
+        called.  Thereafter, all calls return the same instance and are already
+        initialized with that service URL.
+        """
+        if type_webservice_url is None:
+            if not (hasattr(self, 'app_config')
+                    and 'TYPE_WEBSERVICE_URL' in self.app_config):
+                raise RuntimeError('TypeClient has not been initialized')
+        else:
+            self.app_config = {}
+            if not type_webservice_url.endswith('/'):
+                type_webservice_url += '/'  # Avoid a common config problem
+            self.app_config['TYPE_WEBSERVICE_URL'] = type_webservice_url
+            self.examples = yaml.safe_load(DUMMY_CLIENT_YAML)
+            for nm in self.examples:
+                self.examples[nm]['name'] = nm
+            map = {}
+            for k, v in self.examples.items():
+                for nm in v['alt-names']:
+                    safe_nm = nm if isinstance(nm, str) else tuple(nm)
+                    map[safe_nm] = k
+            self.alt_name_map = map
+
+    def getAssayType(self, name: str) -> _AssayType:
+        """
+        Given an assay name, return the associated assay type.  If a deprecated
+        alt-name is provided, the returned assay type will use the up-to-date
+        name.
+        """
+        if name not in self.examples:
+            if name in self.alt_name_map:
+                name = self.alt_name_map[name]
+            else:
+                raise RuntimeError(f'No such assay_type {name},'
+                                   ' even as alternate name')
+        return _AssayType(self.examples[name])
+
+    def iterAssayNames(self, primary: BoolOrNone = None) -> Iterable[str]:
+        """
+        Return an iterator over valid assay name strings.
+
+        primary: controls the subset of valid names, as follows:
+            None or not specified: return all valid names.
+            True: return only the names of primary assay types, that is,
+                those for which no parent is an assay.
+            False: return only the names of non-primary assay types.
+        """
+        if primary is None:
+            for name in self.examples:
+                yield name
+        else:
+            if primary:
+                for name in [nm for nm, v in self.examples.items()
+                             if v['primary']]:
+                    yield name
+            else:
+                for name in [nm for nm, v in self.examples.items()
+                             if not v['primary']]:
+                    yield name
+
+    def iterAssays(self, primary: BoolOrNone = None) -> Iterable[_AssayType]:
+        """
+        Return an iterator over valid assay types.
+
+        primary: controls the subset of valid assay types, as follows:
+            None or not specified: return all valid types.
+            True: return only primary assay types, that is, those for which no
+                parent is an assay.
+            False: return only non-primary assay types.
+        """
+        for name in self.iterAssayNames(primary=primary):
+            yield _AssayType(self.examples[name])
